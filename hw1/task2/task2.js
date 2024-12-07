a1e8e949f6b90daa9bf24a8565d822f1d649479a
viweu <153850831+viweu@users.noreply.github.com>
2024-12-07 19:28:40 +0300

    Update task2.js

--- hw1/task2/task2.js
+++ hw1/task2/task2.js
@@ -1,30 +1,70 @@
-const musicCollection = {
-  albums: [
-    { title: "Huncho Jack", artist: "Travis Scott", year: 2017 },
-    { title: "23", artist: "Central Cee", year: 2022 },
-    { title: "I Am Music", artist: "Lil Wayne", year: 2023 },
-    { title: "Pink Friday", artist: "Nicki Minaj", year: 2020 },
-  ],
+// Повара и их специализации:
 
-  [Symbol.iterator]: function() {
-    let index = 0;
-    const albums = this.albums;
-    return {
-      next: function() {
-        if (index < albums.length) {
-          return {
-            value: albums[index++],
-            done: false,
-          };
-        } else {
-          return { done: true };
-        }
-      },
-    };
-  },
-};
+// Виктор - специализация: Пицца.
+// Ольга - специализация: Суши.
+// Дмитрий - специализация: Десерты.
 
-// Перебор коллекции с помощью цикла for...of
-for (const album of musicCollection) {
-  console.log(`${album.title} - ${album.artist} (${album.year})`);
-}
+let cooks = new Map();
+cooks.set("Виктор", "Пицца")
+    .set("Ольга", "Суши")
+    .set("Дмитрий", "Десерты");
+
+console.log(`Виктор - специализация: ${cooks.get("Виктор")}`);
+console.log(`Ольга - специализация: ${cooks.get("Ольга")}`);
+console.log(`Дмитрий - специализация: ${cooks.get("Дмитрий")}`);
+console.log();
+
+// Блюда и их повара:
+
+// Пицца "Маргарита" - повар: Виктор.
+// Пицца "Пепперони" - повар: Виктор.
+// Суши "Филадельфия" - повар: Ольга.
+// Суши "Калифорния" - повар: Ольга.
+// Тирамису - повар: Дмитрий.
+// Чизкейк - повар: Дмитрий.
+
+let dishes = new Map();
+dishes.set("Пицца 'Маргарита'", "Виктор")
+    .set("Пицца 'Пепперони'", "Виктор")
+    .set("Суши 'Филадельфия'", "Ольга")
+    .set("Суши 'Калифорния'", "Ольга")
+    .set("Тирамису", "Дмитрий")
+    .set("Чизкейк", "Дмитрий");
+
+console.log(`Пиццу 'Маргарита' приготовил повар: ${dishes.get("Пицца 'Маргарита'")}`);
+console.log(`Суши 'Калифорния' приготовил повар: ${dishes.get("Суши 'Калифорния'")}`);
+console.log(`Чизкейк приготовил повар: ${dishes.get("Чизкейк")}`);
+console.log();
+
+// Заказы:
+
+// Клиент Алексей заказал: Пиццу "Пепперони" и Тирамису.
+// Клиент Мария заказала: Суши "Калифорния" и Пиццу "Маргарита".
+// Клиент Ирина заказала: Чизкейк.
+
+let orders = new Map(); //заказы всех клиентов
+
+//первый клиент
+let client1 = { name: 'Алексей'};
+let clientOrder1 = new Set();
+clientOrder1.add("Пицца 'Пепперони'")
+            .add("Тирамису");
+
+//второй клиент
+let client2 = { name: 'Мария'};
+let clientOrder2 = new Set();
+clientOrder2.add("Суши 'Калифорния'")
+            .add("Пицца 'Маргарита'");
+
+//третий клиент
+let client3 = { name: 'Ирина'};
+let clientOrder3 = new Set();
+clientOrder3.add("Чизкейк");
+
+orders.set(client1, clientOrder1) // добавляем заказы клиентов в список
+    .set(client2, clientOrder2)
+    .set(client3, clientOrder3)
+
+    console.log(`Клиент ${client1.name} заказал(а): ${[...orders.get(client1)]}`);
+    console.log(`Клиент ${client2.name} заказал(а): ${[...orders.get(client2)]}`);
+    console.log(`Клиент ${client3.name} заказал(а): ${[...orders.get(client3)]}`);
